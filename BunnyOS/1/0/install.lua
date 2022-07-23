1dd04a35159aa82d1aa84d8e1618292213989488
TheBunnyMan123 <69465699+TheBunnyMan123@users.noreply.github.com>
2022-07-23 12:53:09 -0500

    Create install.lua

---
+++ BunnyOS/1/0/install.lua
@@ -0,0 +1,19 @@
+print("Are you sure you want to install BunnyOS 1.0?")
+if read() == "y" then
+    shell.run("delete","startup")
+    shell.run("delete","startup.lua")
+    shell.run("delete",".menu")
+    shell.run("delete",".getfile")
+    shell.run("delete",".sendfile")
+    shell.run("delete",".user")
+else
+    shell.run("shutdown")
+end
+shell.run("wget","https://raw.githubusercontent.com/TheBunnyMan123/CC-Tweaked-Programs/main/BunnyOS/1/0/startup.lua","/startup.lua")
+shell.run("wget","https://raw.githubusercontent.com/TheBunnyMan123/CC-Tweaked-Programs/main/BunnyOS/1/0/.menu.lua","/.menu")
+shell.run("wget","https://raw.githubusercontent.com/TheBunnyMan123/CC-Tweaked-Programs/main/BunnyOS/1/0/.getfile.lua","/.getfile")
+shell.run("wget","https://raw.githubusercontent.com/TheBunnyMan123/CC-Tweaked-Programs/main/BunnyOS/1/0/.sendfile.lua","/.sendfile")
+print("Success!")
+print("If you are running this from a floppy disk, please remove it now.")
+sleep(10)
+shell.run("reboot")
